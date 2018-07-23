8cc332c2c7cc38dbef1bf149ad7b03ec4ce2b7f7
Steven Daut <stmada@gmail.com>
2018-07-23 09:53:10 -0500

    user mixin stuff

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ app.config['SECRET_KEY'] ='521093fba11f9bd59fa8df97ad5bc902'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager=LoginManager(app)
 
 from flaskblog import routes
