abf60be3c9a3ef32b842de4528508177b05cbca1
Steven Daut <stmada@gmail.com>
2018-07-23 14:05:32 -0500

    fixing file

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -73,7 +73,7 @@ def account():
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
-        flash('Your account has been updated', 'sucess')
+        flash('Your account has been updated', 'success')
         return redirect(url_for('account'))
     elif request.method == 'GET':
         form.username.data = current_user.username
