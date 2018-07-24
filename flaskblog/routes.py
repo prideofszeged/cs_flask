7c549941df258fe13dfbcff5b9412a5697ff53e4
Steve D <stmada@gmail.com>
2018-07-24 07:26:48 -0500

    update stuff

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -109,10 +109,19 @@ def post(post_id):
     return render_template('post.html', title=post.title, post=post)
 
 @login_required
-@app.route("/post/<int:post_id>/update")
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 def update_post(post_id):
     post= Post.query.get_or_404(post_id)
     if post.author != current_user:
         abort(403)
     form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':       
+       form.title.data=post.title
+       form.content.data=post.content
     return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
